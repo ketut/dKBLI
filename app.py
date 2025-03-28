b3f0c788c1668c2e058fcbf876bd797bc60e7027
ketut <ketut@bps.go.id>
2025-03-29 00:38:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ model.to(device)
 # Coba memuat label_encoder (jika ada file lokal)
 try:
     # label_encoder = joblib.load("label_encoder.pkl")
-    label_encoder = joblib.load("label_encoder.pth")
+    # label_encoder = joblib.load("label_encoder.pth")
+    label_encoder = torch.load("label_encoder.pth")
 except FileNotFoundError:
     st.warning("File label_encoder.pkl tidak ditemukan. Menggunakan contoh sementara.")
     kbli_codes = ["47771", "47772", "47773"]  # GANTI DENGAN DAFTAR KODE KBLI ASLI
